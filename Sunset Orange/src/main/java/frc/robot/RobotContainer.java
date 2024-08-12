8f86dcf6cfe6c450be795dfc040e6ee4b04e2e12
Zuoweimena <1037948336@qq.com>
2024-08-12 21:03:13 +0800

    ``` 调整PathPlanner中的最大速度设置
    将默认最大速度和路径中的最大速度约束从4.0调整为0.5，以适应新的机器人速度限制。还调整了maxModuleSpeed设置，以符合这些新的速度约束。
    ```

--- Sunset Orange/src/main/java/frc/robot/RobotContainer.java
+++ Sunset Orange/src/main/java/frc/robot/RobotContainer.java
@@ -157,7 +157,7 @@ public class RobotContainer {
     mChooser = new SendableChooser<>();
     mChooser.addOption("example", new TestAutoCommand(sDrivetrainSubsystem));
     mChooser.addOption("33", new mid33AutoCommand(sDrivetrainSubsystem, sIntaker, sShooter));
-
+    mChooser.addOption("e", new a313332AutoCommand(sDrivetrainSubsystem, sIntaker, sShooter));
     SmartDashboard.putData("AUTO CHOICES", mChooser);
   }
 }
